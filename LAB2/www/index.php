dcc0363372ec1f31e5bc6a0ebcd62b9dc482d195
jokerkung2543 <jokerkung2543@gmail.com>
2020-01-17 12:07:35 +0700

    LAB1

---
+++ LAB2/www/index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+    <form action="php/insert.php" method="POST">
+        <input type="text" name="user" placeholder="USER">
+        <br>
+        <input type="text" name="pass" placeholder="PASS">
+        <br>
+        <button tybe="submit"> Save </button>   
+        <button tybe="reset"> Clear </button>
+
+    </form>
+</body>
+</html>
---
+++ LAB2/www/php/insert.php
@@ -0,0 +1,13 @@
+<?php
+
+include("conDB.php");
+
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+$time = time();
+
+$strSQL = "INSERT INTO tb_login VALUES('$user','$pass','$time')";
+$objRes = mysqli_query($link,$strSQL); 
+
+echo $objRes?$user + " Inserted....":mysqli_error($link);
+?>
